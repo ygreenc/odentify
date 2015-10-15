a2ea5f6c5d419f75e1ab4fe4f9eeecd8c6e36d25
Yanick Chateauvert <yanickc.chateauvert@leclerc.ca>
2015-10-15 11:48:25 -0400

    Initial commit

---
+++ odentify.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import requests
+
+
+def search_movie(movie_name):
+    pass
+
+
+def fetch_movie(movie_name):
+    return requests.get('http://omdbapi.com', params={'s': })
+
+
+def main():
+    pass
